d3ce6c5b7a4b5861d85d0ea36abc24b6728a3402
HumbleFool <sunilkatta830@gmail.com>
2022-10-15 10:02:43 +0530

    Update sunil.c
    Added simple basic c hello program

---
+++ project1/sunil.c
@@ -0,0 +1,7 @@
+#include<stdio.h>  // It is a header file and it contains standard input and outputs.
+int main() // main is a starting function of your code.
+{
+ printf("Hello world!"); //Here printf is a output function and in this language every line ends with "; note it down "
+ return 0;  //return zero is used in at the time of ending 
+//  you can also exit(0)
+}
