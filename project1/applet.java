395664e5565fd697559dcdec4d506ca4c3239bc0
HumbleFool <111487330+HumbleFool830@users.noreply.github.com>
2023-02-04 06:36:02 +0000

    added java applet basic file

---
+++ project1/applet.java
@@ -0,0 +1,16 @@
+package project1;
+
+import java.applet.*;
+import java.awt.*;
+
+class helloworld {
+    @Override
+    public void paint(Graphics g){
+        
+        g.drawString("hello ", 200, 200);
+    }
+    // public static void main(String[] args) {
+    //     helloworld ne=new helloworld();
+    //     ne.paint();
+    // }
+}
